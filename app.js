1f689d1b0124dbb5bf56ca3148ddca95cf688d06
GuljanKG <kiguljan@gmail.com>
2019-09-26 12:02:41 -0600

    Inital commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let followButton = document.getElementById("follow-button");
+
+followButton.addEventListener("click", function() {
+  let socialMediaDiv = document.getElementById("social-media-section");
+  socialMediaDiv.classList.toggle("active");
+});
